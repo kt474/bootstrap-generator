0be0f1f2e18576874f3d4c0b08ca276476851ea1
Kevin Tian <kt474@cornell.edu>
2021-03-13 20:12:47 -0500

    settting up frontend

---
+++ components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- Navbar -->
+  <nav
+    class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4"
+  >
+    <div
+      class="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4"
+    >
+      <!-- Brand -->
+      <a
+        class="text-white text-sm uppercase hidden lg:inline-block font-semibold"
+        href="#"
+        >Canvas</a
+      >
+      <!-- Form -->
+      <form
+        class="md:flex hidden flex-row flex-wrap items-center lg:ml-auto mr-3"
+      >
+        <div class="relative flex w-full flex-wrap items-stretch">
+          <span
+            class="z-10 h-full leading-snug font-normal absolute text-center text-gray-400 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3"
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              /></svg
+          ></span>
+          <input
+            type="text"
+            placeholder="Search here..."
+            class="px-3 py-3 placeholder-gray-400 text-gray-700 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:shadow-outline w-full pl-10"
+          />
+        </div>
+      </form>
+      <!-- User -->
+      <ul class="flex-col md:flex-row list-none items-center hidden md:flex">
+        <div class="items-center flex">
+          <span
+            class="w-12 h-12 text-sm text-white bg-gray-300 inline-flex items-center justify-center rounded-full"
+          >
+            <img
+              alt="..."
+              class="w-full rounded-full align-middle border-none shadow-lg"
+              src="~/assets/me.jpg"
+            />
+          </span>
+        </div>
+      </ul>
+    </div>
+  </nav>
+
+  <!-- End Navbar -->
+</template>
+<script>
+export default {}
+</script>
